f00a63a3bf81769cbeda4489e5c105c0a99fd155
dmitriy30011997 <112504672+dmitriy30011997@users.noreply.github.com>
2023-10-16 17:41:40 +0300

    contacts.xhtml and addContacts created

--- src/main/java/com/PhoneBook/Contact.java
+++ src/main/java/com/PhoneBook/Contact.java
@@ -1,2 +1,48 @@
-package com.PhoneBook;public class Contact {
+package com.PhoneBook;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/main/java/com/PhoneBook/PhoneBookServlet.java
+++ src/main/java/com/PhoneBook/PhoneBookServlet.java
@@ -1,2 +1,23 @@
-package com.PhoneBook;public class PhoneBookServlet {
+package com.PhoneBook;
+
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/phonebook")
+public class PhoneBookServlet extends HttpServlet {
+    @Inject
+    private ContactService contactService;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        List<Contact> contacts = contactService.getAllContacts();
+        // Отправка данных на страницу JSF для отображения
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Обработка создания, обновления или удаления контактов
+    }
 }
